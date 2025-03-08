e902ae441931404f224ecd8f7284375e703ff0da
2020ht66049 <73755609+2020ht66049@users.noreply.github.com>
2025-03-08 22:10:12 +0000

    Update add.py

--- add.py
+++ add.py
@@ -12,3 +12,10 @@ Explaination:
 Conclusion:
 
 This Python program allows users to input two numbers, adds them together, and displays the sum. The use of the input() function to capture user input, the int() function to convert the input to integers, and the print() function to output the result makes the program simple yet effective for basic arithmetic operations.
+
+string reverse
+=============
+
+name = "umesh"
+newname = name[::-1]
+print(newname)
